a2cf914ad5fcfc4e0687f1edb031e9a2432f957d
Pallares <ing.pallares69@gmail.com>
2024-03-07 12:12:38 -0600

    🍻Feature checkbox Biind checkbox with newItwmhighPriority variable

--- src/App.vue
+++ src/App.vue
@@ -16,22 +16,19 @@ const items = ref([
   { id: 4, label: 'Huevos' }
 ]);
 const newItem= ref(''); 
-const newItemPriority = ref('low')
+const newItemHighPriority = ref(false) 
 </script>
 
 <template>
   <h1> <i :class="shoppingIcon">local_mall</i> {{ header }}</h1>
  <input v-model="newItem" type="text" placeholder="Agregar articulo">
- <!-- Radio Buttons -->
- <label ><input type="radio" v-model="newItemPriority" value="low"> Baja</label>
- <label ><input type="radio" v-model="newItemPriority" value="high"> Alta</label>
- <label ><input type="radio" v-model="newItemPriority" value="medium"> No sepo</label>
- {{ newItemPriority === 'low'?  '🍻':'🤢' }}
+<label><input type="checkbox" v-model="newItemHighPriority">Alta pioridad</label>
 
- 
+{{ newItemHighPriority ? '🍻' : '🏆'}}
  <ul>
     <if>
     <li v-for="({id, label}, i) in items" v-bind:key="id" >🍻 {{ label }}🍻</li>
+    
   </if>
 
   </ul>
